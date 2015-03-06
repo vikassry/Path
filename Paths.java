a9b7568e18a57195a27f9e97c50297629ffe4538
Vikas <=>
2015-03-06 20:11:40 +0530

    Version 5 with CLI done wromg file name and wrong -c option handled And version 5 complete

--- Paths.java
+++ Paths.java
@@ -77,21 +77,25 @@ public class Paths {
 		if(args.length>=1 && !args[0].equals("-f")){
 			System.out.println("Invalid option "+args[0]+". Try `-f'"); return;
 		}
-		if(args.length == 4){
+		if(args.length == 6){
 			MyReader r = new MyReader(args[1]);
-			MyReader r2 = new MyReader("cities.txt");
-			Map<String,String> country = r2.getCitiesWithCountries(r2.getContent());
+			MyReader r2 = new MyReader(args[3]);
 			String pathContent = r.getContent();
-			for (String key : country.keySet()) {
-				System.out.println(key+"-> "+country.get(key));
-			}
+			String cityContent = r2.getContent();
 
+			if(!args[2].equals("-c")){
+				System.out.println("Invalid option "+args[2]+". Try `-c'"); return;
+			}
 			if(pathContent.substring(0,6).equals("Error:")){
 				System.out.println("No database named `"+args[1]+"' found."); return;
+			}
+			if(cityContent.substring(0,6).equals("Error:")){
+				System.out.println("No database named `"+args[3]+"' found."); return;
 			}	
 			String[] routes[] = r.getPaths(pathContent);
+			Map<String,String> country = r2.getCitiesWithCountries(r2.getContent());
 			PathManager p = new PathManager(routes,country);
-			System.out.println(p.givePathStatus(args[2], args[3]));
+			System.out.println(p.givePathStatus(args[4], args[5]));
 		}
 		else {
 			String error = " Not enough information.\r\n Please enter the source and destination "+
